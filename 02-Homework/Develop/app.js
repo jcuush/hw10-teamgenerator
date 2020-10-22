572e2aad95fed7a32df8dd08aa45bf48e95be011
jcuush <jrcushing@sbcglobal.net>
2020-10-21 22:03:45 -0500

    updated comments to explain app.js

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -160,7 +160,7 @@ function renderHTML () {
         })
 }
 
-// }
+// COMMENTS FROM INITIAL FILE BELOW:
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
